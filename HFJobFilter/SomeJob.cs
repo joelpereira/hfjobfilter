f68d957b7952e325af4308d16e4aa162f9651b4b
Shahzad Hassan <shahzad.hassan@softmatters.co.uk>
2019-04-27 22:05:40 +0100

    Added DI to the LogDb JobFilter

--- HFJobFilter/SomeJob.cs
+++ HFJobFilter/SomeJob.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace HFJobFilter
+﻿namespace HFJobFilter
 {
-	public class SomeJob
-	{
-		public static string SomeJobMethod()
-		{
-			// do something
-			return "Something was done.";
-		}
-	}
+    public class SomeJob
+    {
+        public static string SomeJobMethod()
+        {
+            // do something
+            return "Something was done.";
+        }
+    }
 }
