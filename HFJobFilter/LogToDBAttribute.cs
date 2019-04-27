6150079b7035020811cbfb47c8b5d92e1524566f
Joel Pereira <joel@goweb-inc.com>
2019-04-26 23:42:35 -0400

    initial

---
+++ HFJobFilter/LogToDBAttribute.cs
@@ -0,0 +1,67 @@
+﻿using Hangfire.Client;
+using Hangfire.Common;
+using Hangfire.Server;
+using Hangfire.States;
+using Hangfire.Storage;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HFJobFilter
+{
+	//public class LogToDbAttribute : JobFilterAttribute,
+	public class LogToDbAttribute : TypeFilterAttribute
+	{
+		private readonly HttpClient _httpClient;
+		public LogToDbAttribute(HttpClient client) : base(typeof(LogToDbImpl))
+		{
+			_httpClient = client;
+		}
+
+
+
+		private class LogToDbImpl : JobFilterAttribute,
+	IClientFilter, IServerFilter, IElectStateFilter, IApplyStateFilter
+		{
+			private readonly HttpClient _httpClient;
+
+			public LogToDbImpl(HttpClient client)
+			{
+				_httpClient = client;
+			}
+
+			public void OnCreating(CreatingContext context)
+			{
+				// log to secondary database..
+			}
+
+			public void OnCreated(CreatedContext context)
+			{
+				string name = context.BackgroundJob.Job.Method.Name;
+			}
+
+			public void OnPerforming(PerformingContext context)
+			{
+			}
+
+			public void OnPerformed(PerformedContext context)
+			{
+			}
+
+			public void OnStateElection(ElectStateContext context)
+			{
+			}
+
+			public void OnStateApplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
+			{
+			}
+
+			public void OnStateUnapplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
+			{
+			}
+		}
+	}
+}
---
+++ HFJobFilter/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HFJobFilter
+{
+	public class Startup
+	{
+		public static readonly HttpClient httpClient = new HttpClient();
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddHangfire(config => config
+				.UseSqlServerStorage(Configuration.GetConnectionString("HangfireDBConnection"))
+				.UseFilter(new TypeFilterAttribute(typeof(LogToDbAttribute)))
+			);
+
+			// just sending something via DI, but this would really be the unit of work
+			services.AddSingleton<HttpClient>(httpClient);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+
+			app.UseHangfireDashboard("/hangfire");
+			app.UseHangfireServer();
+
+
+			AddJob();
+		}
+
+
+		void AddJob()
+		{
+			string jobId = BackgroundJob.Enqueue(() => SomeJob.SomeJobMethod());
+		}
+	}
+}
